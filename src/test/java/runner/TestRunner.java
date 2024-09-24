7584e9236d85751634a5bf022d2bc1ed50feb70c
Marko <marko.shindelic88@gmail.com>
2024-09-24 02:01:32 +0200

    removing redundant code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
         features = "src/test/resources/",
         glue = {"steps"}
-        //plugin = {"pretty", "html:target/cucumber-html-report", "json.cucumber.json"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
