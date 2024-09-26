91ce6d89970fcac615ad0e40a2fb940e048a5b42
MarkoDSindelic <markods@aia.ac>
2024-09-26 11:31:05 +0200

    jenkins file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,8 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        features = "src/test/resources/",
-        glue = {"steps"}
+        features = "src/test/resources/features",
+        glue = {"steps"},
+        plugin = {"json:target/cucumber-json-report.json",
+                "html:target/cucumber-html-report",
+                "junit: target/cucumber.xml"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,12 +1,12 @@
 Feature: Login
-
+@Valid
   Scenario: User can login with valid credentials
     Given User is on the login page
     When User enters username "tomsmith"
     And User enters password "SuperSecretPassword!"
     And User clicks login button
     Then User is logged in
-
+@Invalid
   Scenario: User cannot login with invalid credentials
     Given User is on the login page
     When User enters username "someUsername"
